39726e09c4fe8164c790cf61f7805ceb3f949b78
Awais Mumtaz <awaismumtaz1406@gmail.com>
2026-02-03 17:31:17 +0500

    Refactor remove duplicate logic in C++ program

--- Arrays/removeduplicate.cpp
+++ Arrays/removeduplicate.cpp
@@ -7,13 +7,15 @@ int main(){
 	int a[10]={22,24,22,3333,45,76,44,87};
 	int s=sizeof (a)/sizeof a[0];
 	int x=0;
-for(int i=1; i<s; i++)
+for(int i=1; i<s; i++){
 	if(a[i]!=a[x]){
-		a[++x]=a[i];
+		a[++x]=a[i];}
 	}
+	
 	
 	for(int i=0; i<x; i++){
 		cout<<a[i];}
 	
 	
+
 }
